8d3c082330f9565265e133e2693cfd61941078b4
Hani <tornadofay@gmail.com>
2026-08-29 02:39:53 +0300

    Add explicit AI tool types

---
+++ src/HAgent.Core/Models/AiToolType.cs
@@ -0,0 +1,13 @@
+namespace HAgent.Models
+{
+    public enum AiToolType
+    {
+        BuiltIn = 0,
+        Application = 1,
+        Declarative = 2,
+        UI = 3,
+        SqlServer = 4,
+        MySql = 5,
+        Extension = 6
+    }
+}

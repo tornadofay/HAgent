4fa15c09f84df7d0ffe545b2262ead0a7e124ea4
Hani <tornadofay@gmail.com>
2026-08-31 05:40:47 +0300

    Add provider-neutral structured data query operators

---
+++ src/HAgent.Core/Models/DataQueryOperator.cs
@@ -0,0 +1,17 @@
+namespace HAgent.Models
+{
+    public enum DataQueryOperator
+    {
+        Equal,
+        NotEqual,
+        GreaterThan,
+        GreaterThanOrEqual,
+        LessThan,
+        LessThanOrEqual,
+        StartsWith,
+        Contains,
+        EndsWith,
+        IsNull,
+        IsNotNull
+    }
+}

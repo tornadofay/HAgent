f0311171291ec68739fded342810c181258349a1
Hani <tornadofay@gmail.com>
2026-09-08 18:43:21 +0300

    fix: import policy engine abstraction

--- src/HAgent.Core/Runtime/AiInterventionCoordinator.cs
+++ src/HAgent.Core/Runtime/AiInterventionCoordinator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using HAgent.Abstractions;
 using HAgent.Models;
 
 namespace HAgent.Runtime

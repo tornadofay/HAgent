b69bbaa4cb6d96c8df4d6d60dcc299ed1baf9742
tornadofay <tornadofay@gmail.com>
2026-09-08 10:20:48 +0300

    using fix

--- src/HAgent.WinForms/Forms/PolicyEditorForm.cs
+++ src/HAgent.WinForms/Forms/PolicyEditorForm.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using HAgent.Abstractions;
 using HAgent.Models;
+using HAgent.Runtime;
 using HAgent.WinForms.Controls;
 using HAgent.WinForms.Helpers;
 using HAgent.WinForms.Helpers.Button;

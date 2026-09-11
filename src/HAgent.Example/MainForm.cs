a7fd02dc533f8db8b2471fcd15364fb44aa90df2
Hani <tornadofay@gmail.com>
2026-09-11 08:25:49 +0300

    Register 0.9575 Slice 8 learning lifecycle Example

--- src/HAgent.Example/MainForm.cs
+++ src/HAgent.Example/MainForm.cs
@@ -77,6 +77,7 @@ namespace HAgent.Example
             AddEventFeatureTabs();
             AddPolicyTabs();
             AddLearningPolicyTab();
+            AddLearningLifecycleTab();
             AddApprovalWorkflowTab();
             AddInstructionContractsTab();
             AddExecutionInterventionTab();

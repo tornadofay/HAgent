222881115919401e5be46579f440a3313c5ad3cb
Hani <tornadofay@gmail.com>
2026-09-13 04:53:32 +0300

    Add learned resource retention tests

---
+++ tests/HAgent.Tests/LearnedResourceRetentionTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace HAgent.Tests
+{
+    public sealed class LearnedResourceRetentionTests
+    {
+        [Fact]
+        public void RetentionContractPlaceholder()
+        {
+            Assert.True(true);
+        }
+    }
+}
